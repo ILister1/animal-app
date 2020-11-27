9373d2f3e95958a2acc51c34b139f9d83f964a32
ILister1 <ilister@qa.com>
2020-11-27 16:35:23 +0000

    containerised testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-sudo apt install python3 python3-pip python3-venv -y
 
-python3 -m venv testing-venv
+sudo docker build -t testing-img -f testing/Dockerfile .
+sudo docker run -it -d --name testing-cont testing-img
 
-. testing-venv/bin/activate
+sudo docker exec testing-cont pytest ./service1 --cov ./service1
+sudo docker exec testing-cont pytest ./service2 --cov ./service2
 
-pip3 install -r ./requirements.txt
-
-cd service1
-pytest --cov .
-cd ..
-cd service2
-pytest --cov .
-cd ..
-
-deactivate
-
-rm -rf testing-venv
+sudo docker stop testing-cont
+sudo docker rm testing-cont
